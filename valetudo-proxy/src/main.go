77666df12049935f7f35c25ee9e50e37c5e9b48e
Wouter Valk <wvalk@wvalk.nl>
2026-01-21 21:17:20 +0100

    add start logging

--- valetudo-proxy/src/main.go
+++ valetudo-proxy/src/main.go
@@ -20,6 +20,19 @@ func main() {
 		log.Fatalf("Failed to parse target URL: %v", err)
 	}
 
+	log.Printf(`Starting with the following settings
+TargetURL: %s
+Basic_auth: %s
+Username: %s
+Password: %t`,
+		targetURL,
+		os.Getenv("basic_auth"),
+		os.Getenv("username"),
+		func() bool {
+			return len(os.Getenv("password")) > 0
+		}(),
+	)
+
 	proxy := httputil.NewSingleHostReverseProxy(target)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
